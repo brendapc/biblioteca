5c1737a95e073f0df9843f033fae085109266e84
Brenda Pereira <brendacamara@outlook.com>
2023-08-28 22:51:10 -0300

    add sql

---
+++ BibliotecaTurma30/src/main/resources/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO livros (codigo,titulo,autor,ano) VALUES (10,'Introducao ao JAVA','Huguinho Pato',2022)
+INSERT INTO livros (codigo,titulo,autor,ano) VALUES (20,'Introducao ao Sptring-Boot','Zezinho Pato',2020)
+INSERT INTO livros (codigo,titulo,autor,ano) VALUES (15,'Principios SOLID','Luizinho Pato',2023)
+INSERT INTO livros (codigo,titulo,autor,ano) VALUES (17,'Padroes de projeto','Lala Pato',2019)
