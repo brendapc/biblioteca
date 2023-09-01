f8c33727bf6486db13ee22c82a90b14f147efd02
Brenda Pereira <brendacamara@outlook.com>
2023-09-01 09:10:44 -0300

    add methods to acervoBanco

--- BibliotecaTurma30/src/main/java/com/bcopstein/endpointsdemo1/AcervoBanco.java
+++ BibliotecaTurma30/src/main/java/com/bcopstein/endpointsdemo1/AcervoBanco.java
@@ -1,6 +1,8 @@
 package com.bcopstein.endpointsdemo1;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,6 +13,76 @@ public class AcervoBanco implements IAcervoRepository {
     @Autowired
     public AcervoBanco(JdbcTemplate jdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Set<String> getAutoresAno(int ano) {
+        return jdbcTemplate.query("SELECT autor FROM Livros WHERE ano = ?",
+                                  (rs, rowNum) -> rs.getString("autor"),
+                                  ano)
+                           .stream()
+                           .collect(Collectors.toSet());
+    }
+
+    @Override
+    public List<Livro> getTitulo (String titulo) {
+        return jdbcTemplate.query("SELECT * FROM Livros WHERE titulo = ?",
+                                  (rs, rowNum) -> new Livro(rs.getLong("codigo"),
+                                                            rs.getString("titulo"),
+                                                            rs.getString("autor"),
+                                                            rs.getInt("ano")),
+                                  titulo);
+    }
+
+    @Override
+    public List<String> getTitulosAutorAno(String autor, int ano) {
+        return jdbcTemplate.query("SELECT titulo FROM Livros WHERE autor = ? AND ano = ?",
+                                  (rs, rowNum) -> rs.getString("titulo"),
+                                  autor,ano);
+    }
+
+    @Override
+    public Set<String> getAutores() {
+        return jdbcTemplate.query("SELECT autor FROM Livros",
+                                  (rs, rowNum) -> rs.getString("autor"))
+                           .stream()
+                           .collect(Collectors.toSet());
+    }
+
+    @Override
+    public List<String> getTitulos() {
+        return jdbcTemplate.query("SELECT titulo FROM Livros",
+                                  (rs, rowNum) -> rs.getString("titulo"));
+    }
+
+    @Override
+    public List<Livro> getAuthor(String autor) {
+        return jdbcTemplate.query("SELECT * FROM Livros WHERE autor = ?",
+                                  (rs, rowNum) -> new Livro(rs.getLong("codigo"),
+                                                            rs.getString("titulo"),
+                                                            rs.getString("autor"),
+                                                            rs.getInt("ano")),
+                                  autor);
+    }
+
+    @Override
+    public Livro getPorId(Long codigo) {
+        return jdbcTemplate.queryForObject("SELECT * FROM Livros WHERE codigo = ?",
+                                            (rs, rowNum) -> new Livro(rs.getLong("codigo"),
+                                                                      rs.getString("titulo"),
+                                                                      rs.getString("autor"),
+                                                                      rs.getInt("ano")),
+                                            codigo);
+    }
+
+    @Override
+    public List<Livro> getAno(int ano) {
+        return jdbcTemplate.query("SELECT * FROM Livros WHERE ano = ?",
+                                  (rs, rowNum) -> new Livro(rs.getLong("codigo"),
+                                                            rs.getString("titulo"),
+                                                            rs.getString("autor"),
+                                                            rs.getInt("ano")),
+                                  ano);
     }
 
     @Override
